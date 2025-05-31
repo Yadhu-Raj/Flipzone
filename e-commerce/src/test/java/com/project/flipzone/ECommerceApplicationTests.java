de036b303f8c64e7c66ef67ab7dd91a5bda609d5
Yadhu <yadhuraj130799@gmail.com>
2025-05-31 16:58:52 +0530

    Initial Commit

---
+++ e-commerce/src/test/java/com/project/flipzone/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.flipzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
