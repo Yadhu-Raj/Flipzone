cf6a07c7e63c5cd0f308269c1b2d91d827459397
Yadhu <yadhuraj130799@gmail.com>
2025-06-15 12:43:35 +0530

    Some edits

--- e-commerce/src/main/java/com/project/flipzone/controller/RestControllerFilpzone.java
+++ e-commerce/src/main/java/com/project/flipzone/controller/RestControllerFilpzone.java
@@ -4,7 +4,6 @@ import com.project.flipzone.flipzoneDTO.UserDTO;
 import com.project.flipzone.service.FlipzoneService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,11 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1/users")
 @RestController
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 public class RestControllerFilpzone {
 
-    @Autowired
-    private  FlipzoneService flipzoneService;
+    private final FlipzoneService flipzoneService;
 
     @PostMapping("/adduser")
     public ResponseEntity<String> addUser (@Valid @RequestBody UserDTO user) throws Exception {
